21b146eb942337bea4fe1b6d468753e4c6258dae
Lawrence Caulker <locaulker@gmail.com>
2025-04-27 18:51:43 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "The Let Them Theory",
+    img: "./images/book-1.jpg",
+    id: 1
+  },
+
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "./images/book-2.jpg",
+    id: 2
+  }
+]
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        const { img, title, author, id } = book
+        return <Book img={img} title={title} author={author} key={id} />
+      })}
+    </section>
+  )
+}
+
+const Book = ({ img, author, title }) => {
+  // console.log(props)
+  // const { author, title, img } = props
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<BookList />)
