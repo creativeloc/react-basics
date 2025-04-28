533b1e30e2ef465a04fe4487638ad4270b7c1ce1
creativeloc <creativeloc@yahoo.com>
2025-04-28 01:27:45 -0400

    introduce basic events

--- src/index.js
+++ src/index.js
@@ -22,10 +22,36 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
         // const { img, title, author, id } = book
         return <Book {...book} key={book.id} />
       })}
+    </section>
+  )
+}
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("Handle Form Input")
+  }
+
+  const handleButtonClick = () => {
+    alert("Handle Button Click")
+  }
+
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click Me</button>
     </section>
   )
 }
